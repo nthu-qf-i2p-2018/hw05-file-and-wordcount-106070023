e1ddfad6d982f74c9eeafb1d5eb7bcc302ff3cf9
106070023 <k0722k22@gmail.com>
2018-04-22 12:37:26 +0800

    first

--- hw5.py
+++ hw5.py
@@ -1,52 +1,34 @@
-# -*- coding: utf-8 -*-
-import ...
+import csv
+import json
+import pickle
+import string
 
 
 def main(filename):
-    # read file into lines
-    lines = ...
-
-    # declare a word list
+    file=open(filename)
+    lines =file.readlines()
     all_words = []
-
-    # extract all words from lines
+    exclude=set(string.punctuation)
+    exclude.remove("'")
     for line in lines:
-        # split a line of text into a list words
-        # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
-
-        # check the format of words and append it to "all_words" list
+        words =line.split()
         for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
-            word = ...
-            # check if word is not empty
+            word =''.join(a for a in word if a not in exclude)
             if word:
-                # append the word to "all_words" list
-                all_words...
-
-    # compute word count from all_words
-    counter = ...
-
-    # dump to a csv file named "wordcount.csv":
-    # word,count
-    # a,12345
-    # I,23456
-    # ...
-    with open(...) as csv_file:
-        # create a csv writer from a file object (or descriptor)
-        writer = ...
-        # write table head
+                all_words.append(word)
+    w=list(set(all_words))
+    wordcount=[]
+    for n in range(len(w)):
+        counter =all_words.count(w[n])
+        wordcount.append((w[n],counter))
+    with open('wordcount.csv','w',newline='') as csv_file:
+        writer = csv.writer(csv_file)
         writer.writerow(['word', 'count'])
-        # write all (word, count) pair into the csv writer
-        writer.writerows(...)
-
-    # dump to a json file named "wordcount.json"
-    ...
-
-    # BONUS: dump to a pickle file named "wordcount.pkl"
-    # hint: dump the Counter object directly
-
+        writer.writerows(wordcount)
+    
+    json.dump(wordcount, open("wordcount.json",'w'))
+    pickle.dump(wordcount, open("wordcount.pkl", 'wb'))
+    
 
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
