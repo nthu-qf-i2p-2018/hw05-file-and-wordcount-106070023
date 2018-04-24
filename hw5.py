f9e259eef6d226c4b8d496ccbc746dae038362f0
106070023 <k0722k22@gmail.com>
2018-04-24 16:28:37 +0800

    second

--- hw5.py
+++ hw5.py
@@ -8,23 +8,18 @@ def main(filename):
     file=open(filename)
     lines =file.readlines()
     all_words = []
-    exclude=set(string.punctuation)
-    exclude.remove("'")
     for line in lines:
         words =line.split()
         for word in words:
-            word =''.join(a for a in word if a not in exclude)
+            word=word.strip(string.punctuation)
             if word:
                 all_words.append(word)
-    w=list(set(all_words))
-    wordcount=[]
-    for n in range(len(w)):
-        counter =all_words.count(w[n])
-        wordcount.append((w[n],counter))
+    from collections import Counter
+    wordcount=Counter(all_words)
     with open('wordcount.csv','w',newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['word', 'count'])
-        writer.writerows(wordcount)
+        writer.writerows(wordcount.most_common())
     
     json.dump(wordcount, open("wordcount.json",'w'))
     pickle.dump(wordcount, open("wordcount.pkl", 'wb'))
